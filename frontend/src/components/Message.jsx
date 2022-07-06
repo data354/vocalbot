0fea844438f19b3d3da9b2fdff6cb6dccda2ba5f
Dougban Monsia <monsia.dougban@data354.co>
2022-07-06 14:53:29 +0000

    feat: add voice recorder in frontend

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -2,37 +2,52 @@ import "../style/Message.css";
 import userLogo from "../assets/user-icon.png"
 import djamo from "../assets/djamo.png"
 import ReactMarkdown from "react-markdown";
+//import { format } from "timeago.js";
 
-
-function Message({text, createdAt, own }) {
+function Message({text, audio, createdAt, own }) {
   if (own){
-    return (
-            <div className="message own">
+    if (text){
+      return (<div className="message own">
                 <div className="messageTop">
-                    <div className="messageText"><ReactMarkdown>{text}</ReactMarkdown></div>
-                    <img
+                  <p className="messageText"><ReactMarkdown>{text}</ReactMarkdown></p>
+                  <img
                     className="messageImg"
                     src={userLogo}
                     alt=""
-                    />
+                  />
                 </div>
                 <div className="messageBottom">{createdAt}</div>
-            </div>
-        )
+              </div>
+            )
+    } else {
+      return (<div className="message own">
+                <div className="messageTop">
+                  <audio
+                      id='audio'
+                      controls
+                      src={audio.url}
+                  ></audio>
+                  <img
+                    className="messageImg"
+                    src={userLogo}
+                    alt=""
+                  />
+                </div>
+                <div className="messageBottom">{createdAt}</div>
+              </div>)
+    }
   } else{
-    return (
-        <div className="message">
-        <div className="messageTop">
-            <img
-            className="messageImg"
-            src={djamo}
-            alt=""
-            />
-            <p className="messageText"><ReactMarkdown>{text}</ReactMarkdown></p>
-        </div>
-        <div className="messageBottom">{createdAt}</div>
-        </div>
-    )
+    return (<div className="message">
+      <div className="messageTop">
+        <img
+          className="messageImg"
+          src={djamo}
+          alt=""
+        />
+        <p className="messageText"><ReactMarkdown>{text}</ReactMarkdown></p>
+      </div>
+      <div className="messageBottom">{createdAt}</div>
+    </div>)
   }
 }
 export default Message;
--- frontend/src/components/Messenger.jsx
+++ frontend/src/components/Messenger.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { io } from "socket.io-client";
+import Message from "./Message";
 import { useState, useEffect, useRef } from "react";
 import {PaperAirplaneIcon} from "@heroicons/react/outline"
-import "../style/Messenger.css"
-import Message from "./Message";
-import { io } from "socket.io-client";
+import AudioReactRecorder, { RecordState } from 'audio-react-recorder'
+import axios from "axios";
 
 const socket = io.connect("http://0.0.0.0:5005")
-
+const voiceApi = axios("localhost:8000")
 export default function Messenger(params) { 
     const scrollRef = useRef(); // for auto scrolling
     // conversations stock all conversations
@@ -15,6 +16,7 @@ export default function Messenger(params) {
     const onSentMessage = (event) => {
         // This function is actioned when user sent message
         //
+        event.stopPropagation()
         const today = new Date();
         // const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
         const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
@@ -24,7 +26,7 @@ export default function Messenger(params) {
             [...conversations, {id: "user_uttered", text: user_uttered, createdAt: time}]
         )
         document.getElementById("input_message").value = ""
-        socket.emit("user_uttered",{"message": user_uttered})
+       socket.emit("user_uttered",{"message": user_uttered})
     }
 
     socket.on("bot_uttered", (response) => {
@@ -43,43 +45,104 @@ export default function Messenger(params) {
       }, [conversations]);
 
     const onPressEnter = (event) => {
+        event.stopPropagation()
         if(event.code === "Enter"){
             onSentMessage(event)
         }
     }
-   
-    return (       
-        <div className="bg-gray-200 p-5 flex justify-center h-5/6">
-            <div className="bg-gray-100 w-1/2 flex flex-col shadow-2xl rounded-xl">
-                <div id="output_message" className="rounded-t-2xl flex-1 overflow-y-scroll scroll-auto p-5">
-                    {
-                        conversations.map(
-                            (
-                                {id, text, createdAt}, index) => (
+
+    // AUDIO MODULE SCRIPT
+    const [recordState, setRecordState] = useState(null);
+    const [audioData, setAudioData] = useState(null);
+    const [displayWave, setDisplayWave] = useState(false);
+    const start = () => {
+        setDisplayWave(true)
+        setRecordState(RecordState.START)
+    }
+    const pause = () => {setRecordState(RecordState.PAUSE)}
+    const stop = () => {setRecordState(RecordState.STOP)}
+    const onStop = (data) => {
+        setAudioData(data)
+        setDisplayWave(false)
+        const today = new Date();
+        // const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        setConversations(
+            [...conversations, {id: "user_uttered", audio: data, createdAt: time}]
+        )
+        
+        var formData = new FormData();
+        formData.append("audio", data.blob);
+        console.log(formData)
+        console.log(data)
+
+        // voiceApi.post("/sound/send", formData, {
+        //     headers: {
+        //         "Content-type": "multipart/form-data"
+        //     }
+        // })
+    }
+    const hidden = displayWave? null: "hidden"
+    const show = displayWave? "hidden": null
+    
+    return (
+        <>
+            <div className="bg-gray-200 p-5 flex justify-center h-5/6">
+                <div className="bg-gray-100 w-1/2 flex flex-col shadow-2xl rounded-xl">
+                    <div id="output_message" className="rounded-t-2xl flex-1 overflow-y-scroll scroll-auto p-5">
+                        {
+                            conversations.map(
+                                ({id, text, audio, createdAt}, index) => (
                                     <div ref={scrollRef} key={`${id}-${index}`}>
                                         <Message 
                                             text={text}
+                                            audio={audio}
                                             createdAt={createdAt}
                                             own={id==="user_uttered"} />
                                     </div>
                                 )
-                            )
-                    }
-                </div>
-                <div className="flex p-3 bg-slate-500" >
-                        <input
-                        id="input_message"
-                        className="w-full p-3 flex-1 rounded-3xl"
-                        type="text"
-                        name="input"
-                        placeholder="Message..."
-                        onKeyUp={onPressEnter}
-                        />
-                    <div onClick={onSentMessage} >
-                        <PaperAirplaneIcon className="rounded-2xl h-12 rotate-90 cursor-pointer pl-1" />
+                                )
+                        }
+                    </div>                              
+                    <div className="flex p-3 h-20 bg-slate-500" >
+                        <div className={`flex flex-1 ${show}`}>
+                            <input
+                            id="input_message"
+                            className="w-full p-3 flex-1 rounded-3xl"
+                            type="text"
+                            name="input"
+                            placeholder="Message..."
+                            onKeyUp={onPressEnter}
+                            />
+                            <div onClick={onSentMessage} >
+                                <PaperAirplaneIcon className="rounded-2xl h-12 rotate-90 cursor-pointer pl-1" />
+                            </div>
+                        </div>
+                        <div className={`flex flex-1 py-1 px-5 ${hidden}`}>
+                            <AudioReactRecorder
+                            state={recordState}
+                            onStop={onStop}
+                            backgroundColor='rgb(100 116 139)'
+                            foregroundColor='rgb(255 255 255)'
+                            canvasWidth={490}
+                            canvasHeight={55}
+                            />                  
+                        </div>  
+                        <div className="flex">
+                            <button id='record' onClick={start} className="bg-green-500 w-12 shadow-3xl rounded-md mx-5">
+                            Start
+                            </button>
+                            <button id='pause' onClick={pause} className="bg-yellow-300 w-12 shadow-3xl rounded-md">
+                            Pause
+                            </button>
+                            <button id='stop' onClick={stop} className="bg-red-500 w-12 shadow-3xl rounded-md mx-5">
+                            Stop
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            
+        </>
     )
 }
